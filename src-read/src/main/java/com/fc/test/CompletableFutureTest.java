0c05895a206036f884d2d430968b6bd548f41c2b
Fchen <fc_2012@163.com>
2023-01-16 17:33:36 +0800

    feat: 练习 Future API

---
+++ src-read/src/main/java/com/fc/test/CompletableFutureTest.java
@@ -0,0 +1,27 @@
+package com.fc.test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author: Fchen
+ * @date: 2021/1/6 3:24 下午
+ * @desc: 测试类
+ */
+public class CompletableFutureTest {
+
+    public static ConcurrentHashMap hashMap = new ConcurrentHashMap<>();
+    static {
+        hashMap.put("1", 0);
+        hashMap.put("2", 0);
+    }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<Void> runAsync = CompletableFuture.runAsync(() -> System.out.println(123));
+
+        CompletableFuture<Integer> supplyAsync = CompletableFuture.supplyAsync(() -> 100)
+                .thenApply(i -> i * 10)
+                .thenApplyAsync(i -> i + 1000);
+        System.out.println(supplyAsync.get());
+    }
+}
